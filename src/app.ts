a3b65875ef37d43c197668df82c132c30c87a492
Ayush Yadav <ayushyadavbusiness1@gmail.com>
2026-03-22 12:04:04 +0530

    Updated backend

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express" ;
 import userRoutes from "./routes/userRoutes"
 import dotenv  from "dotenv";
+import cors from "cors"
+
 
 
 dotenv.config();
@@ -9,6 +11,12 @@ const PORT = process.env.PORT ;
 const app = express() ;
 
 app.use(express.json());
+app.use(cors({
+    origin :["http://localhost:5000"],
+    methods :["GET","PUT","POST","PATCH","DELETE"],
+    credentials : true
+
+}))
 
 app.use("/api/" , userRoutes) ;
 
